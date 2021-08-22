9c08d94b22d918bc8b59664fc66661c5e04c55e9
Gabriel Cesarino <gahnrq@hotmail.com>
2021-08-22 19:59:53 -0300

    add: Avatar component to HeaderOptions

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import { Home, SupervisorAccount } from '@material-ui/icons'
+import { BusinessCenter, Chat, Home, Notifications, SupervisorAccount } from '@material-ui/icons'
 import SearchIcon from '@material-ui/icons/Search'
 import React from 'react'
 import './Header.css'
@@ -17,7 +17,12 @@ function Header() {
 
             <div className="header__right">
                 <HeaderOption Icon={Home} title="Home"/>
-                <HeaderOption Icon={SupervisorAccount}title="My Network"/>
+                <HeaderOption Icon={SupervisorAccount} title="My Network"/>
+                <HeaderOption Icon={BusinessCenter} title="Jobs"/>
+                <HeaderOption Icon={Chat} title="Messaging"/>
+                <HeaderOption Icon={Notifications} title="Notifications"/>
+                <HeaderOption avatar="https://i.pinimg.com/736x/02/5c/86/025c86e60f2b348153d6beb4696378ce.jpg" title="Sponge Bob"
+                />
             </div>
         </div>
     )
