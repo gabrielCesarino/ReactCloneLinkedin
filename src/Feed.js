f529aa66804ac41ca7352e1796839d49f95df50c
Gabriel Cesarino <gahnrq@hotmail.com>
2021-08-24 08:28:26 -0300

    add: inputOptions style

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
-import { Create } from '@material-ui/icons'
+import { CalendarViewDay, Create, EventNote, Image, Subscriptions } from '@material-ui/icons'
 import React from 'react'
 import './Feed.css'
+import InputOption from './InputOption'
 
 function Feed() {
     return (
@@ -12,6 +13,12 @@ function Feed() {
                         <input type="text"/>
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption title='Photo' Icon={Image} color="#7085f9" />
+                    <InputOption title='Video' Icon={Subscriptions} color="#E7A33E" />
+                    <InputOption title='Event' Icon={EventNote} color="#C0CBCD" />
+                    <InputOption title='Write Article' Icon={CalendarViewDay} color="#7FC15E" />
                 </div>
             </div>
         </div>
